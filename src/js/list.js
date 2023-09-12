994ff0753a1803f47a45753b9d1ff55b57de31ae
th0-Kim <3001th0@naver.com>
2023-09-13 00:21:35 +0900

    js수정 및 내용 업데이트

--- src/js/list.js
+++ src/js/list.js
@@ -1,5 +1,5 @@
 /*
- * Version   : ver 0.1.0
+ * Version   : ver 0.1.2
  * File      : js/index.js
  * Author    : KIM SO YOUNG
  *
@@ -11,28 +11,55 @@ const appBody = document.body;
 const list_item = '.list_item';
 const listTit = '.item_title';
 const listInfo = '.item_back';
+const listThumb = '.thumb_box';
 const ACTIVE_CLASS = 'on';
 
 let app = {
   listInit: function () {
     this.thisitem = appBody.querySelectorAll(list_item);
-    app.listEvent();
+    app.init();
   },
-  listEvent: function () {
-    const items = this.thisitem;
-    for (const item of items) {
-      item.addEventListener('mouseenter', app.enterFunc);
-      item.addEventListener('mouseleave', app.leaveFunc);
+  init: function () {
+    if (window.navigator.userAgent.match(/Android|iPhone|iPad|iPod/i)) {
+      app.moEvent();
+    } else {
+      app.pcEvent();
     }
   },
-  enterFunc: function () {
-    this.classList.add(ACTIVE_CLASS);
-    const itemHeight = this.querySelector(listTit).clientHeight;
-    this.querySelector(listInfo).style.height = itemHeight + 80 + 'px';
+  moEvent: function () {
+    const items = this.thisitem;
+    for (const item of items) {
+      item.addEventListener('click', app.clickFunc);
+    }
+  },
+  pcEvent: function () {
+    const items = this.thisitem;
+    for (const item of items) {
+      item.addEventListener('mouseleave', app.leaveFunc);
+      item.addEventListener('mouseenter', app.enterFunc);
+    }
   },
   leaveFunc: function () {
-    this.classList.remove(ACTIVE_CLASS);
-    this.querySelector(listInfo).style.height = '';
+    app.reset(this);
+  },
+  enterFunc: function () {
+    app.set(this);
+  },
+  clickFunc: function () {
+    if (this.classList.contains(ACTIVE_CLASS)) {
+      app.reset(this);
+    } else {
+      app.set(this);
+    }
+  },
+  reset: function (e) {
+    e.classList.remove(ACTIVE_CLASS);
+    e.querySelector(listInfo).style.height = '';
+  },
+  set: function (e) {
+    e.classList.add(ACTIVE_CLASS);
+    const itemHeight = e.querySelector(listTit).clientHeight;
+    e.querySelector(listInfo).style.height = itemHeight + 80 + 'px';
   },
 };
 
@@ -42,3 +69,12 @@ function hasJqueryObject(elem) {
 
 // 조건이 맞으면 Go!
 hasJqueryObject(appBody.querySelector(list_item)) && app.listInit(list_item);
+
+// go to top!
+const topButton = document.querySelector('.top');
+if (topButton) {
+  topButton.addEventListener('click', scrollTop);
+}
+function scrollTop() {
+  window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+}
