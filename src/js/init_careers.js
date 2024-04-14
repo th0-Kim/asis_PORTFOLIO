f9bda705f2b3f101c5025b6aadafac5ff38d8219
th0-Kim <ksyy3001@naver.com>
2024-04-14 23:25:27 +0900

    기여 내용 추가

--- src/js/init_careers.js
+++ src/js/init_careers.js
@@ -31,21 +31,25 @@ function setCareersUl(data) {
     const periodProject = project.list;
     periodProject.forEach((list, i) => {
       const ulAreaList = document.createElement('li'); //career_list 자식 li 생성
+      const ulAreaFlex = document.createElement('div'); //career_list 기간 생성
       const ulAreaPeriod = document.createElement('strong'); //career_list 기간 생성
-      const ulAreaProject = document.createElement('span'); //career_list 프로젝트명 생성
+      const ulAreaProject = document.createElement('p'); //career_list 프로젝트명 생성
       const ulAreaTextName = document.createElement('span'); //career_list 프로젝트명 생성
       const ulAreaTexKind = document.createElement('span'); //career_list 유형 생성
       //const ulAreaTextRate = document.createElement('span'); //career_list 참여율 생성
+      const ulAreaContent = document.createElement('div'); //career_list 기여 내용 생성
 
       ulContainer.appendChild(ulArea); //문서에 생성한 ul 추가
       ulArea.appendChild(ulAreaList); //career_list > li 추가
-      ulAreaList.appendChild(ulAreaPeriod); //career_list > li > li_tite 추가
-      ulAreaList.appendChild(ulAreaProject); //career_list > li > li_text 추가
+      ulAreaList.appendChild(ulAreaFlex); //career_list > li > div 추가
+      ulAreaFlex.appendChild(ulAreaPeriod); //career_list > li > strong 추가
+      ulAreaFlex.appendChild(ulAreaProject); //career_list > li > p 추가
       ulAreaProject.appendChild(ulAreaTextName); //career_list > li > p > span 추가
       ulAreaProject.appendChild(ulAreaTexKind); //career_list > li > p > span 추가
       //ulAreaProject.appendChild(ulAreaTextRate); //career_list > li > p > span 추가
 
       ulArea.className = 'career_list'; //생성한 ul에 career_list 클래스 부여
+      ulAreaFlex.className = 'flex'; //생성한 div에 flex 클래스 부여
       ulAreaPeriod.className = 'li_title'; //생성한 strong에 li_title 클래스 부여
       ulAreaProject.className = 'li_text'; //생성한 p에 li_text 클래스 부여
       ulAreaTextName.className = 'name'; //생성한 span에 name 클래스 부여
@@ -55,6 +59,12 @@ function setCareersUl(data) {
       ulAreaPeriod.textContent = list.period;
       ulAreaTextName.textContent = list.project;
       ulAreaTexKind.textContent = list.kind;
+
+      if (list.content !== '') {
+        ulAreaList.appendChild(ulAreaContent); //career_list > li > div
+        ulAreaContent.className = 'li_content'; //생성한 div에 li_content 클래스 부여
+        ulAreaContent.textContent = list.content;
+      }
       //ulAreaTextRate.textContent = '참여율 : ' + list.rate + '%';
     });
   });
